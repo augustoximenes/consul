7e813c68a6e53394c94a5b3f60bdd2b73e36319a
Augusto Ximenes <augustoximenes@gmail.com>
2021-05-25 19:38:44 -0300

    adicionado os 2 serviços sidecar e configurada variável local_bind_address

--- apps/frontend/5_config_app.sh
+++ apps/frontend/5_config_app.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-echo "Starting Sidecar Service ..."
-sudo cp /vagrant/apps/frontend/consul-sidecar.service /etc/systemd/system
-sudo systemctl enable consul-sidecar
-sudo systemctl start consul-sidecar
+echo "Starting Sidecar Service - web..."
+sudo cp /vagrant/apps/frontend/consul-sidecar-web.service /etc/systemd/system
+sudo systemctl enable consul-sidecar-web
+sudo systemctl start consul-sidecar-web
+
+echo "Starting Sidecar Service - nginx..."
+sudo cp /vagrant/apps/frontend/consul-sidecar-nginx.service /etc/systemd/system
+sudo systemctl enable consul-sidecar-nginx
+sudo systemctl start consul-sidecar-nginx
